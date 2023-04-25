184151572535bd9d3dcdb5a452942c7f1ea88e91
shahs <shahs@192.168.56.1>
2023-04-25 23:23:35 +0500

    small fix

--- project/scrapper/src/main/java/ru/tinkoff/edu/java/scrapper/repository/jdbc/LinkJdbcTemplateRepository.java
+++ project/scrapper/src/main/java/ru/tinkoff/edu/java/scrapper/repository/jdbc/LinkJdbcTemplateRepository.java
@@ -1,15 +1,17 @@
 package ru.tinkoff.edu.java.scrapper.repository.jdbc;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Primary;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 import ru.tinkoff.edu.java.scrapper.mapper.LinkRowMapper;
-import ru.tinkoff.edu.java.scrapper.model.Link;
+import ru.tinkoff.edu.java.scrapper.model.commonDto.Link;
+import ru.tinkoff.edu.java.scrapper.repository.jdbcAndJooqContract.LinkRepository;
 
 import java.sql.Timestamp;
 import java.util.List;
 
-@Repository
+
 @Slf4j
 public class LinkJdbcTemplateRepository implements LinkRepository {
 
@@ -26,14 +28,14 @@ public class LinkJdbcTemplateRepository implements LinkRepository {
 
     @Override
     public List<Link> findAll() {
-        log.info("findAll() method invocation in linkRepo");
+        log.info("findAll() method invocation in linkJdbcRepo");
         String sql = "select * from link";
         return jdbcTemplate.query(sql, linkRowMapper);
     }
 
     @Override
     public Link findByUrl(String url) {
-        log.info("findByUrl() method invocation in linkRepo");
+        log.info("findByUrl() method invocation in linkJdbcRepo");
         String sql = "select * from link where link.url = ?";
         List<Link> link = jdbcTemplate.query(sql, linkRowMapper, url);
         return link.size() == 0 ? null : link.get(0);
@@ -41,22 +43,22 @@ public class LinkJdbcTemplateRepository implements LinkRepository {
 
     @Override
     public void add(Link link) {
-        log.info("add() method invocation in linkRepo");
-        String sql = "insert into link (url, updated_at) values(?, ?)";
-        jdbcTemplate.update(sql, link.getUrl(), link.getUpdatedAt());
+        log.info("add() method invocation in linkJdbcRepo");
+        String sql = "insert into link (url, checked_at) values(?, ?)";
+        jdbcTemplate.update(sql, link.getUrl(), link.getCheckedAt());
     }
 
 
     @Override
-    public void updateDate(Link link) {
-        log.info("updateDate() method invocation in linkRepo");
-        String sql = "update link set updated_at = ? where id = ?";
-        jdbcTemplate.update(sql, link.getUpdatedAt(), link.getId());
+    public void updateCheckDate(Link link) {
+        log.info("updateDate() method invocation in linkJdbcRepo");
+        String sql = "update link set checked_at = ? where id = ?";
+        jdbcTemplate.update(sql, link.getCheckedAt(), link.getId());
     }
 
     @Override
     public void remove(Long id) {
-        log.info("remove() method invocation in linkRepo");
+        log.info("remove() method invocation in linkJdbcRepo");
         String sql = "delete from link where link.id = ?";
         jdbcTemplate.update(sql, id);
     }
@@ -64,9 +66,24 @@ public class LinkJdbcTemplateRepository implements LinkRepository {
     @Override
     //поиск ссылок по критерию
     public List<Link> findOldLinks(Long timeUpdateDelta) {
-        log.info("findOldLinks() method invocation in linkRepo");
+        log.info("findOldLinks() method invocation in linkJdbcRepo");
         Timestamp compareDate = new Timestamp(System.currentTimeMillis() - timeUpdateDelta*1000);
-        String sql = "select * from link where link.updated_at < ? order by link.updated_at desc";
+        String sql = "select * from link where link.checked_at < ? order by link.checked_at desc";
         return jdbcTemplate.query(sql,linkRowMapper,compareDate);
     }
+
+    @Override
+    public void updateGhLink(Link link) {
+        log.info("updateGhLink() method invocation in linkJdbcRepo");
+        String sql = "update link set gh_forks_count = ?, gh_description = ?, gh_pushed_at = ? where id = ?";
+        jdbcTemplate.update(sql, link.getGhForksCount(), link.getGhDescription(), link.getGhPushedAt(), link.getId());
+    }
+
+    @Override
+    public void updateSoLink(Link link) {
+        log.info("updateSoLastEditDate() method invocation in linkJdbcRepo");
+        String sql = "update link set so_last_edit_date = ?, so_answer_count = ? where id = ?";
+        jdbcTemplate.update(sql, link.getSoLastEditDate(), link.getSoAnswerCount(), link.getId());
+    }
+
 }
